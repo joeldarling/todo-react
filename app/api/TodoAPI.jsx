07cfc9f76fd9b9859261ffc72c681996bafdfb4a
Joel <joel.darling@gmail.com>
2016-08-17 21:28:37 -0400

    text filter completed

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -23,9 +23,21 @@ module.exports = {
     filteredTodos = filteredTodos.filter((todo)=>{
       return !todo.completed || showCompleted;
     });
-    // filter by searchText
 
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo)=>{
+      return searchText.length === 0 || todo.text.indexOf(searchText) > -1;
+    });
     //sort todos with non complete first
+    filteredTodos.sort((a,b)=>{
+      if(!a.completed && b.completed){
+        return -1;
+      } else if (a.completed && !b.completed){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
     return filteredTodos;
   }
 };
